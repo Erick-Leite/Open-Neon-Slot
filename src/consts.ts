7aa0fedd983c93b134a7c3e0552fd324779a83f0
erickleite338 <erickleite338@gmail.com>
2025-09-16 17:49:16 +0000

    feat(consts): Add site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,4 @@
 export const SITE_TITLE = "Open Neon Slot";
+export const SITE_DESCRIPTION =
+  "Mergulhe na emoção do Open Neon Slot, um jogo de caça-níquel gratuito e de código aberto, feito para fins educacionais.";
 export const AUTHOR = "Pedro Erick";
